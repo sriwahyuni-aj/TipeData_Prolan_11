ea0c89926c0c5349bd17c8095a8589816dc6e998
sriwahyuni-aj <ajsriwahyuni@gmail.com>
2017-03-08 23:23:28 +0700

    Add files via upload

---
+++ Tipedata.java
@@ -0,0 +1,62 @@
+public class Tipedata{
+//Tipe Data Objek
+	public static void main(String [] args){
+	String nama = "Brlajar Java Yuks";
+	System.out.println(nama.length());
+	
+//Tipe Data Byte
+	byte maxByte = 127;
+	byte minByte = -128;
+	byte defaultByte = 0;
+	System.out.println(defaultByte);
+	
+//Tipe Data short
+	short maxShort =32767;
+	short minShort =-32768;
+	short defaultShort = 0;
+	System.out.println(defaultShort);
+	
+//Tipe data INT
+	int maxInt = 2147483647;
+	int minInt = -2147483648;
+	int defaultInt = 0;
+	System.out.println(defaultInt);
+
+//Tipe Data Long
+	long maxLong = 9223372036854775807L;
+	long minLong = -9223372036854775808L;
+	long defaultLong = 0L;
+	System.out.println(defaultLong);
+	
+//Tipe Data FLOAT
+	float maxFloat = 3.4028234799999999999999999999999999999f;
+	float minFloat = -3.4028234799999999999999999999999999999f;
+	System.out.println(maxFloat);
+	System.out.println(minFloat);
+	
+// Tipe Data Double
+	double randomDouble = 1.797693134862315702d;
+	double randomDouble1 = -1.797693134862315602d;
+	double defaultDouble = 0 ;
+	System.out.println(randomDouble);
+	
+//TIPE DATA BOOLEAN 
+	boolean menikah = false;
+	System.out.println(menikah);
+	
+//TIPE DATA CHAR
+	char A = 'A';
+	System.out.println(A);
+	
+//TIPE DATA STRING
+	String nama1 = "ADE BAGUS SANDRY";
+	String alamat = "ds.Tanjungsari Kec.cikaum Kab.Subang Prov.JawaBarat";
+	System.out.println(nama1.length());
+	System.out.println(alamat.length());
+	
+
+
+  }
+
+
+}
